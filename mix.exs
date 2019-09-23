4003571aebb4336e7a6925d9c43a57b1a2a25506
Ed Bond <ed.bond@mere-tech.com>
2019-09-23 00:04:49 -0500

    generating documentation

--- mix.exs
+++ mix.exs
@@ -35,7 +35,9 @@ defmodule Sucrose.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:absinthe, "~> 1.4.0"}
+      {:absinthe, "~> 1.4.0"},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false}
+
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
